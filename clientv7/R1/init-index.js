e22327523e694a4c62ccec4bfee7f4dd4c813bdc
Ashraf Souleiman <Ashraf@cedrusco.com>
2017-11-09 20:44:05 -0500

    CZAR Commit

---
+++ clientv7/R1/init-index.js
@@ -0,0 +1,40 @@
+config = require('./configuration/config');
+var app = require('connect')();
+var http = require('http');
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+
+module.exports = {
+    init: init
+};
+
+function init(originalApp, info) {
+    try {
+        // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+        var spec = fs.readFileSync(__dirname + '/swaggerConfig/input.yaml', 'utf8');
+        var swaggerDoc = jsyaml.safeLoad(spec);
+
+        // Initialize the Swagger middleware
+        swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
+
+            // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+            app.use(middleware.swaggerMetadata());
+
+            // Validate Swagger requests
+            app.use(middleware.swaggerValidator());
+
+            // Serve the Swagger documents and Swagger UI
+            app.use(middleware.swaggerUi());
+            // Route validated requests to appropriate controller
+            app.use(middleware.swaggerRouter(config.swaggerRouterOptions));
+
+            var apiurl = info.apiPath;
+
+            originalApp.use(apiurl, app);
+
+        });
+    } catch (error) {
+        throw error;
+    }
+}
